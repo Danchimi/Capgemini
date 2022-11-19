fc57fa0b3e987d870924f67c51c3a3126df8f106
Danchimi <daniellechimi@yahoo.com>
2022-11-19 08:11:35 +0100

    startseite, wizzard , fragen

--- src/app/secondpage/secondpage.component.ts
+++ src/app/secondpage/secondpage.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-secondpage',
   templateUrl: './secondpage.component.html',
   styleUrls: ['./secondpage.component.css']
+   
 })
 export class SecondpageComponent implements OnInit {
 
